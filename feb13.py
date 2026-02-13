b1d6b2be2324d7c2f3cc646e72c57e01d1602cb8
GR <gr@aca.com>
2026-02-13 14:35:47 -0500

    hi

---
+++ feb13.py
@@ -0,0 +1,25 @@
+print("hello")
+age = 17
+print(age)
+print(8 + 3)
+print(8 - 3)
+print(8 * 3)
+radius = 4.5
+pi = 3.1415
+area = pi * (radius ** 2)
+print("the area of the circle is:", area)
+print(3 + 4 * 2)
+first_name = "Ada"
+last_name = "Lovelace"
+print(first_name,last_name)
+name = "Ada Lovelace"
+print(f"{name} was a pioneer.")
+is_student = True
+print(10 > 7)
+print(10 < 7)
+print(10 == 7)
+print(10 != 7)
+age = 18
+has_ticket = True
+print(age >= 18)
+print(has_ticket)
