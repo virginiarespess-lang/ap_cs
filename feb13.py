8af7a7cd9fd5f4847a098b20659c6e1a0732deb2
GR <gr@aca.com>
2026-02-20 14:17:27 -0500

    ji

--- feb13.py
+++ feb13.py
@@ -23,3 +23,32 @@ age = 18
 has_ticket = True
 print(age >= 18)
 print(has_ticket)
+temperature = 72
+is_raining = False
+if temperature >= 70 and not is_raining:
+    print("it is a good day for a picnic")
+else:
+    print("it is a bad day for a picnic")
+score1 = 85
+score2 = 92
+average = (score1 + score2) / 2
+print(f"average = {average}")
+if average > 90:
+    print("The average is above 90")
+else: 
+    print("The average is below 90")
+print(score1 > score2)
+print(f"{score1} > {score2}")
+gpa = 3.6
+has_recommendation = True
+if gpa >= 3.5 and has_recommendation:
+    print("you qualify!")
+else:
+    print("you dont qualify.")
+print(f"(5 * 2 > 9) is {True}")
+print(f"(10 / 2 == 5) is {True}") 
+truth = True
+lie = False
+print("truth + lie")
+price = 19.999
+print(f"The total = ${price + 0.001}")
---
+++ feb21.py
@@ -0,0 +1,7 @@
+print("Logic")
+count = 0
+print(count)
+count = count + 1
+print(count)
+print(11 -2)
+print(11 % 2)
