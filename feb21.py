c4eb6ad4f4958a390a2ca705b67768d87180c3ec
GR <gr@aca.com>
2026-02-20 14:36:14 -0500

    ji

--- feb21.py
+++ feb21.py
@@ -5,3 +5,25 @@ count = count + 1
 print(count)
 print(11 -2)
 print(11 % 2)
+base = 4
+base3 = base ** 3
+print(base3)
+subject = "Computer"
+category = "Science"
+print(subject, category)
+x = 45
+y = 50
+print(x != y)
+print(x > 50)
+in_stock = True
+on_sale = True
+if in_stock and on_sale:
+    print("Buy!")
+else:
+    print("Dont Buy!")
+print((10 + 2 > 11) or (5 - 1 == 0))
+depth = 15
+width = 10
+height = 2
+volume = depth * width * height
+print("the volume is:", volume)
